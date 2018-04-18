b29fe706a038a4063fcb7c84fb992fad5c6934ee
Boris Benchev <boris.benchev@bbc.co.uk>
2018-04-18 19:48:55 +0100

    Simplify classes and avoid using default props

--- FoodApp/DetailedRecipe.swift
+++ FoodApp/DetailedRecipe.swift
@@ -8,25 +8,22 @@
 
 import UIKit
 
-// A simple class to represent a Recipe
+// A simple class to represent a DetailedRecipe
 class DetailedRecipe: Recipe {
-    var bigImageUrl = "https://lh3.googleusercontent.com/Q8n3i7QbQZUqeSggcgEEocDh0_t4ymFsjNq6_LPbNCBDJn_Ppyq50Sg2lHzT8OuGul_qrEWjvddJK4K8XfWR=s360";
-    var ingredients: [String] = ["1 (8oz) box Zatarain's Yellow Rice, cooked according to package.", "4 boneless skinless chicken breasts."];
-    var prepTime = "15 Min";
-    var cookTime = "7 Hr";
-    var numberOfServings = "8";
+    let bigImageURL: String;
+    let ingredients: [String];
+    let prepTime: String;
+    let cookTime: String;
+    let numberOfServings: String;
     
-    override init() {
-        super.init();
-    }
     
-    init(rId: String, rName: String, rSmallImageUrl: String, rBigImageUrl: String, rIngredients: [String], rPrepTime: String, rCookTime: String, rNumberOfServings: String) {
-        bigImageUrl = rBigImageUrl;
-        ingredients = rIngredients;
-        prepTime = rPrepTime;
-        cookTime = rCookTime;
-        numberOfServings = rNumberOfServings;
-        super.init(rId, rName, rSmallImageUrl);
+    init(id: String, name: String, smallImageURL: String, bigImageURL: String, ingredients: [String], prepTime: String, cookTime: String, numberOfServings: String) {
+        self.bigImageURL = bigImageURL;
+        self.ingredients = ingredients;
+        self.prepTime = prepTime;
+        self.cookTime = cookTime;
+        self.numberOfServings = numberOfServings;
+        super.init(id: id, name: name, smallImageURL: smallImageURL);
     }
 }
 
--- FoodApp/Recipe.swift
+++ FoodApp/Recipe.swift
@@ -10,18 +10,13 @@ import UIKit
 
 // A simple class to represent a Recipe
 class Recipe {
-    // Hardcode fields for initial testing purposes
-    var id = "Crockpot-Cheesy-Chicken-_-Rice-2132197";
-    var name = "Crockpot Cheesy Chicken & Rice";
-    var smallImageUrl = "https://lh3.googleusercontent.com/Q8n3i7QbQZUqeSggcgEEocDh0_t4ymFsjNq6_LPbNCBDJn_Ppyq50Sg2lHzT8OuGul_qrEWjvddJK4K8XfWR=s90";
+    let id: String;
+    let name: String;
+    let smallImageURL: String
     
-    init() {
-        
-    }
-    
-    init(_ rId: String, _ rName: String, _ rSmallImageUrl: String) {
-        id = rId;
-        name = rName;
-        smallImageUrl = rSmallImageUrl;
+    init(id: String, name: String, smallImageURL: String) {
+        self.id = id;
+        self.name = name;
+        self.smallImageURL = smallImageURL;
     }
 }
