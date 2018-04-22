fc6abd3f9b8060567158faad349d4f1e5a8e8c90
Boris Benchev <boris.benchev@bbc.co.uk>
2018-04-22 01:21:28 +0100

    Modify non guaranteed DetailedRecipe properties to be Optional

--- FoodApp/Models/DetailedRecipe.swift
+++ FoodApp/Models/DetailedRecipe.swift
@@ -11,20 +11,29 @@ import UIKit
 // A simple class to represent a DetailedRecipe
 class DetailedRecipe {
     let name : String
-    let imageURL: String;
-    let ingredients: [String];
-    let prepTime: String;
-    let cookTime: String;
-    let numberOfServings: Int;
+    let imageURL: String?
+    let ingredients: [String]
+    let prepTime: String?
+    let cookTime: String?
+    let numberOfServings: Int?
+    let sourceURL: String?
     
-    
-    init(_ id: String, _ name: String, _ imageURL: String, _ ingredients: [String], _ prepTime: String, _ cookTime: String, _ numberOfServings: Int) {
+    /**
+     @param name             - Recipe name
+     @param imageURL         - Optional String as it might not be present in API response
+     @param ingredients      - Array of Strings with recipe ingredients
+     @param prepTime         - Recipe preparation time. Optional as it might not be present in API response
+     @param cookTime         - Recipe cooking time. Optional as it might not be present in API response
+     @param numberOfServings - Number of servings for the recipe. Optional as it might not be present in API response
+    */
+    init(_ name: String, _ imageURL: String?, _ ingredients: [String], _ prepTime: String?, _ cookTime: String?, _ numberOfServings: Int?, _ sourceURL: String?) {
         self.name = name
-        self.imageURL = imageURL;
-        self.ingredients = ingredients;
-        self.prepTime = prepTime;
-        self.cookTime = cookTime;
-        self.numberOfServings = numberOfServings;
+        self.imageURL = imageURL
+        self.ingredients = ingredients
+        self.prepTime = prepTime
+        self.cookTime = cookTime
+        self.numberOfServings = numberOfServings
+        self.sourceURL = sourceURL
     }
 }
 
